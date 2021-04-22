8b084b5149ef309eb74c8833b089300191eb49f5
senpai-10 <bmjfdrh@gmail.com>
2021-04-22 05:56:01 +0300

    HandleCmk

--- src/cmk.js
+++ src/cmk.js
@@ -1,1 +1,22 @@
 // create master key
+const editJsonFile = require("edit-json-file");
+const { encrypt, decrypt } = require("../crpyto")
+let config = editJsonFile(`/home/senpai/.upm`, {autosave: true})
+
+document.getElementById("cmk-submit").addEventListener("click", HandleCmk);
+
+function HandleCmk() {
+    let input = document.getElementById('masterkey-input').value
+    let input_retype = document.getElementById('masterkey-input-retype').value
+    
+    if (input) {
+        if (input == input_retype) {
+            config.set("MasterKey", encrypt(input))
+            window.location = "login.html"
+        } else {
+            alert("wrong password")
+        }
+    } else {
+        alert("no input")
+    }
+}
