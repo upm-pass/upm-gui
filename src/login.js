f8634a15308515a0b27ee69d657c6fa593ac182d
senpai-10 <bmjfdrh@gmail.com>
2021-04-22 05:56:40 +0300

    login

--- src/login.js
+++ src/login.js
@@ -2,19 +2,17 @@ const editJsonFile = require("edit-json-file");
 const { encrypt, decrypt } = require("../crpyto")
 let config = editJsonFile(`/home/senpai/.upm`, {autosave: true})
 
-
 document.getElementById("login").addEventListener("click", HandleLogin);
 
 function HandleLogin() {
     let input = document.getElementById('masterkey-input').value
-    if (!input) {
-        alert("no input")
-    } else {
+    if (input) {
         if (input == decrypt(config.get("MasterKey"))) {
-            console.log("logged in")
             window.location = "index.html"
         } else {
             alert("wrong password")
         }
+    } else {
+        alert("no input")
     }
 }
