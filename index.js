6218aa3e90b2e63ba410ce98e43874456ddafbad
senpai-10 <bmjfdrh@gmail.com>
2021-04-22 05:09:54 +0300

    fix spaces

--- index.js
+++ index.js
@@ -2,29 +2,27 @@ const {app, BrowserWindow} = require('electron')
 const path = require('path')
 
 function createWindow () {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-        nodeIntegration: true,
-        contextIsolation: false,
-        enableRemoteModule: true,
-        preload: path.join(__dirname, 'preload.js')
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+            preload: path.join(__dirname, 'preload.js')
     }
-  })
+    })
 
-  mainWindow.loadFile(path.join(__dirname, 'src/ui/login.html'))
-
+    mainWindow.loadFile(path.join(__dirname, 'src/ui/login.html'))
 }
 
 app.whenReady().then(() => {
-  createWindow()
-  
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+    createWindow()
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
 })
 
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+    if (process.platform !== 'darwin') app.quit()
 })
