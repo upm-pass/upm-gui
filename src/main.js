ac8568d2592a08223d56b35a7a58a01052960e2d
senpai-10 <bmjfdrh@gmail.com>
2021-04-23 07:20:29 +0300

    get data

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const editJsonFile = require("edit-json-file");
+const { encrypt, decrypt } = require("../crpyto")
 let config = editJsonFile(`/home/senpai/.upm`, {autosave: true})
 
 document.getElementById("remove-all").addEventListener("click", RemoveAll);
@@ -18,3 +19,29 @@ function RemoveAll () {
         document.getElementById('remove-all').innerHTML = oldValue
     })
 }
+
+function LoadPasswords () 
+{
+    let data = config.get("passwords")
+    var count = data.length;
+    var container = document.getElementById("passwords");
+    var Element;
+
+    // console.log(data);
+
+    for (var key in data) {
+        // if (data.hasOwnProperty(key)) {
+            console.log(key + " -> " + data[key])
+            console.log(decrypt(config.get(`passwords.${key}.password`)))
+        // }
+    }
+
+    // for (var i = 0; i < count; i++) {
+    //     var item = data[i];
+    //     Element = ``
+        
+    //     container.innerHTML += Element;
+    // }
+}
+
+LoadPasswords()
