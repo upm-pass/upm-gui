c8e84af8229c6a0b166954d56cd446c6c6f95d26
senpai-10 <bmjfdrh@gmail.com>
2021-05-16 00:09:38 +0300

    default_config

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const path = require('path')
 const editJsonFile = require("edit-json-file")
 let config_file = editJsonFile(`/home/${require("os").userInfo().username}/.config/upm/config`, {autosave: true})
 let config = editJsonFile(config_file.get("upm_path"), {autosave: true})
+const default_config = {
+    "notification_position": "top_right"
+}
+
+if (!config_file.get("settings")) {
+    config_file.set("settings", default_config)
+}
 if (!config_file.get("upm_path")) {
     config_file.set("upm_path", `/home/${require("os").userInfo().username}/.config/upm/.upm`)
 }
