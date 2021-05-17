080d9d52d0299b668114765cf1f9fb02fe4b1ae5
senpai-10 <bmjfdrh@gmail.com>
2021-05-17 07:13:54 +0300

    rename

--- src/lib/main/keybinds.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("keydown", (key) => {
-    if (key.ctrlKey && key.key == 'f') {
-        document.getElementById("search").focus()
-    }
-    if (key.ctrlKey && key.key == 'd') {
-        if (!document.getElementById("prompt"))
-            add()
-    }
-})
